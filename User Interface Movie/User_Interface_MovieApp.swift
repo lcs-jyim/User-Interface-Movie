3703ace578e360184b58f2edab151995eef77ece
Josh Yim <jyim@lcs.on.ca>
2023-11-12 00:44:40 -0500

    Initial Commit

---
+++ User Interface Movie/User_Interface_MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  User_Interface_MovieApp.swift
+//  User Interface Movie
+//
+//  Created by junxi Yim on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct User_Interface_MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
