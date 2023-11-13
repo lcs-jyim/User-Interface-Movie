d11caf9fb7b7506b9d07f6535b9670e04259218f
Josh Yim <jyim@lcs.on.ca>
2023-11-13 02:00:44 -0500

    created the linenumberrestriction feature to restrict the number of lines that names take

--- User Interface Movie/ContentView.swift
+++ User Interface Movie/ContentView.swift
@@ -18,8 +18,8 @@ struct ContentView: View {
                         
                         .bold()
                     HStack(alignment: .top){
-                        MovieInformation(movieName: "Outlander", imageFileTitle: "OutlanderPoster", dateCreated: "N/A", DateVisibility: 0)
-                        MovieInformation(movieName: "Star Trek: Strange New Worlds", imageFileTitle: "StartrekPoster", dateCreated: "N/A", DateVisibility: 0)
+                        MovieInformation(movieName: "Outlander", imageFileTitle: "OutlanderPoster", dateCreated: "N/A", DateVisibility: 0, lineNumberRestriction: 3)
+                        MovieInformation(movieName: "Star Trek: Strange New Worlds", imageFileTitle: "StartrekPoster", dateCreated: "N/A", DateVisibility: 0, lineNumberRestriction: 3)
                     }
                     Divider()
                     VStack(alignment: .leading){
@@ -28,12 +28,58 @@ struct ContentView: View {
                             .padding(.bottom,1)
                             .bold()
                         HStack(alignment: .top){
-                            MovieInformation(movieName: "Outlander", imageFileTitle: "OutlanderPoster", dateCreated: "N/A", DateVisibility: 0)
-                            MovieInformation(movieName: "Star Trek: Strange New Worlds", imageFileTitle: "StartrekPoster", dateCreated: "N/A", DateVisibility: 0)
+                            MovieInformation(movieName: "South Park: Joining the Panderverse", imageFileTitle: "SouthParkPoster", dateCreated: "October 27", DateVisibility: 1, lineNumberRestriction: 2)
+                            MovieInformation(movieName: "Five Nights  at Freddy's", imageFileTitle: "FiveNightsPoster", dateCreated: "October 27", DateVisibility: 1, lineNumberRestriction: 2)
+                            MovieInformation(movieName: "PAW Patrol:The Mighty Movie", imageFileTitle: "PawPatrolPoster", dateCreated: "September 29", DateVisibility: 1, lineNumberRestriction: 2)
+                            MovieInformation(movieName: "Saw X", imageFileTitle: "SawXPoster", dateCreated: "September 29", DateVisibility: 1, lineNumberRestriction: 2)
                         }
                     }
+                    VStack(alignment: .leading){
+                        Text("Popular TV Shows")
+                            .font(.system(size: 30))
+                            .padding(.bottom,5)
+                            .bold()
+                        
+                        ZStack {
+                            HStack{
+                                MovieInformation(movieName: "RandomTV1", imageFileTitle: "Random1", dateCreated: "N/A", DateVisibility: 0, lineNumberRestriction: 1)
+                                MovieInformation(movieName: "RandomTV2", imageFileTitle: "Random2", dateCreated: "N/A", DateVisibility: 0, lineNumberRestriction: 1)
+                                MovieInformation(movieName: "RandomTV3", imageFileTitle: "Random3", dateCreated: "N/A", DateVisibility: 0, lineNumberRestriction: 1)
+                            }
+                            .blur(radius:10)
+                            Rectangle()
+                                .foregroundColor(.backgroundGray)
+                                .opacity(0.8)
+                            
+                            
+                            ZStack {
+                                Rectangle()
+                                    .frame(width:350,height:50)
+                                    .foregroundColor(.gray)
+                                    .cornerRadius(20)
+                                    .opacity(0.4)
+                                
+                                HStack{
+                                    Image(systemName: "magnifyingglass")
+                                        .resizable()
+                                        .frame(width:25,height:25)
+                                        .foregroundColor(.white)
+                                    Text("9 free searches remaining")
+                                        .foregroundColor(.searchbarGray)
+                                        .opacity(0.8)
+                                    Spacer()
+                                }
+                                .offset(x:30)
+                            }
+                            .offset(y:-50)
+                            
+                        }
+    
+                    }
+                    
                     Spacer()
                 }
+                .offset(x:35,y:100)
             }
             .navigationTitle("Discover")
             .navigationBarTitleDisplayMode(.inline)
@@ -53,6 +99,9 @@ struct ContentView: View {
                     }
             }
                 .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+                .ignoresSafeArea()
+                .frame(width:394,height:730)
+                .clipped()
             }
         }
     }
--- User Interface Movie/Preview Content/Small Movie Structure.swift
+++ User Interface Movie/Preview Content/Small Movie Structure.swift
@@ -13,28 +13,30 @@ struct MovieInformation: View {
     let imageFileTitle:String
     let dateCreated:String
     var DateVisibility:Double
+    let lineNumberRestriction:Int
     
     
     var body: some View {
         VStack(alignment:.leading){
             Image(imageFileTitle)
                 .resizable()
-                .frame(width: 120,height: 180)
+                .frame(width: 110,height: 160)
                 .cornerRadius(5.0)
             Text(movieName)
-                .font(.system(size: 20))
+                .font(.system(size: 15))
                 .padding(.leading,5)
                 .fixedSize(horizontal: false, vertical: true)
+                .lineLimit(lineNumberRestriction)
             Text(dateCreated)
-                .font(.system(size: 15))
+                .font(.system(size:10 ))
                 .foregroundColor(.gray)
                 .opacity(DateVisibility)
                 .padding(.leading,5)
             }
-        .frame(width:120)
+        .frame(width:110)
         }
     }
     
     #Preview {
-        MovieInformation(movieName: "abababa", imageFileTitle: "Settings Icon", dateCreated: "Oct 27", DateVisibility: 1)
+        MovieInformation(movieName: "abababa", imageFileTitle: "Settings Icon", dateCreated: "Oct 27", DateVisibility: 1, lineNumberRestriction: 2)
     }
