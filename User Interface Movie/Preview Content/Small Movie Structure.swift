340389461c8016b0b7536a2bd0839735ff27bc1a
Josh Yim <jyim@lcs.on.ca>
2023-11-12 22:57:18 -0500

    Created some colors( the purple that is the main color in the interface

---
+++ User Interface Movie/Preview Content/Small Movie Structure.swift
@@ -0,0 +1,18 @@
+//
+//  Small Movie Structure.swift
+//  User Interface Movie
+//
+//  Created by junxi Yim on 2023/11/12.
+//
+
+import SwiftUI
+
+struct Small_Movie_Structure: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Small_Movie_Structure()
+}
---
+++ User Interface Movie/Preview Content/portions of different subjects.swift
@@ -0,0 +1,18 @@
+//
+//  portions of different subjects.swift
+//  User Interface Movie
+//
+//  Created by junxi Yim on 2023/11/12.
+//
+
+import SwiftUI
+
+struct portions_of_different_subjects: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    portions_of_different_subjects()
+}
